85f8bbdd30988ae5b3a5f18f6235af94dd4ed2f9
AdmiraLahav <lavhrmn@gmail.com>
2026-06-28 11:41:18 +0300

    Update voice-chat.py

--- voice-chat.py
+++ voice-chat.py
@@ -17,7 +17,7 @@ async def join(ctx):
     if (ctx.author.voice):
         channel = ctx.message.author.voice.channel
         voice = await channel.connect()
-        source = FFmpegPCMAudio("./Beginning In The End.mp3")
+        source = FFmpegPCMAudio("Beginning In The End.mp3")
 
         print("Playing song")
         player = voice.play(source)
